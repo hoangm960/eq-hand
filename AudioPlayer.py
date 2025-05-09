0c323bd368a5740921e8e82ff8c7263ae881e995
My <ltquemy@gmail.com>
2025-05-09 14:51:25 +0700

    [dev] commit volume control

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -13,6 +13,7 @@ class AudioPlayer:
         self.samples = np.array(self.audio.get_array_of_samples()).astype(
             np.float32) / 32768.0
         self.fs = 44100
+        self.volume = 1.0  # default volume 100%
         self.buffer_pos = 0
         self.stop_playback = False
         self.buffer_lock = threading.Lock()
@@ -22,14 +23,29 @@ class AudioPlayer:
             outdata[:] = np.zeros((frames, 1))
             raise sd.CallbackStop()
             return
+
         with self.buffer_lock:
             chunk = self.samples[self.buffer_pos:self.buffer_pos + frames]
             self.buffer_pos += frames
+        # apply eq
         eq_chunk = au.apply_eq(chunk, self.fs, self.eq_controller.get_gains())
+        # adjust volume
+        eq_chunk *= self.volume
+        eq_chunk = np.clip(eq_chunk, -1.0, 1.0)
+
         outdata[:] = eq_chunk.reshape(-1, 1)
 
+    def set_volume(self, new_volume):
+        with self.buffer_lock:
+            self.volume = max(0.0, min(new_volume, 2.0))
+
+    def get_volume(self):
+        return self.volume
+
     def play(self):
-        with sd.OutputStream(callback=self.callback, channels=1, samplerate=self.fs, blocksize=1024):
+        with sd.OutputStream(callback=self.callback,
+                             channels=1, samplerate=self.fs,
+                             blocksize=1024):
             sd.sleep(int(len(self.samples) / self.fs * 1000))
 
     def stop(self):
